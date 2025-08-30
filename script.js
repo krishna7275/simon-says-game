5b1c83703145a632e31f3913a34e5da27d666bad
krishna7275 <kpy.yt9833067464@gmail.com>
2025-08-30 12:10:13 +0530

    some imp baki hai

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let gameSeq =[];
 let UserSeq = [];
+let btns = ["yellow","red","green","purple"];
+
  let started = false;
  let level = 0;
 
@@ -12,14 +14,35 @@ let UserSeq = [];
         levelUp();
  });
 
-function btnFlash(){
-    btnFlash,classList.add("flash");
-    ti
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(() => {
+         btn.classList.remove("flash");
+    }, 250);
 }
 
  function levelUp(){
     level++;
     h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 4);
+    let randColor = btns[randIdx];
+    let randbtn = document.querySelector(`.${randColor}`);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randbtn); 
+    btnFlash(randbtn);
+ }
+
+function btnPress(){
+    // console.log(this)
+    let btn = this;
+    btnFlash(btn)
+}
+
+let allBtns = document.querySelectorAll(".btn")
+ for (btn of allBtns ){
+    btn.addEventListener("click",btnPress);
  }
 
 
